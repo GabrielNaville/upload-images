0947f978027bcf57a33be99aabeaf6218b4a76ff
gabriel <gabriel.naville@pontaltech.com.br>
2020-11-10 08:39:36 -0300

    subindo alteraçao para que o upload funcione

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const routes = require('express').Router();
 
 routes.use('/', home);
 
-routes.get('/api', api);
+routes.use('/api', api);
 
 routes.get('/ping', (req, res) => {
     res.status(200).send('pong');
